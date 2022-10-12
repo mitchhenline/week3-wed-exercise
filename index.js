f71db88f2dbd807350d49c994e575fdf485811ff
Mitch Henline <rmhenline@gmail.com>
2022-10-12 16:56:20 -0600

    4.5 done

---
+++ index.js
@@ -0,0 +1,34 @@
+function addMovie(event) {
+    event.preventDefault()
+    let inputField = document.querySelector("input").value
+    let movie = document.createElement("li")
+    let movieTitle = document.createElement("span")
+  
+    movieTitle.textContent = inputField
+    movie.appendChild(movieTitle)
+    document.querySelector('ul').appendChild(movie)
+  
+    let deleteBtn = document.createElement("button")
+    deleteBtn.textContent = "X"
+    movie.appendChild(deleteBtn)
+    deleteBtn.addEventListener("click", deleteMovie)
+
+    span.textContent.addEventListener('click', crossOffMovie)
+  
+    document.querySelector("input").value = ""
+
+    inputField = "" //figure this out for later
+}
+  
+function deleteMovie(event) {
+  event.target.parentNode.remove()
+  console.log("hit delete movie")
+}
+
+document.querySelector('form').addEventListener("submit", addMovie)
+
+function crossOffMovie(event) {
+    event.target.classList.toggle('.checked')
+}
+
+
